1778b65801547953892ff0de9bdfbcd6f824ac00
Fulwin <vinty_email@163.com>
2019-01-25 11:46:07 +0800

    Move user model to models folder

--- app/Http/Controllers/SummariesController.php
+++ app/Http/Controllers/SummariesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Summary;
+use App\Models\Summary;
 
 class SummariesController extends Controller
 {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('account')->unique()->comment('账号');
+            $table->string('name')->comment('用户姓名');
+            $table->string('email')->unique()->comment('邮箱');
+            $table->string('password', 60)->comment('密码');
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->tinyInteger('status')->default(1)->comment('状态');
+            $table->tinyInteger('gender')->default(1)->comment('性别 0:女 1:男');
+            $table->integer('department_id')->nullable()->comment('部门id');
+            $table->integer('post_id')->nullable()->comment('职位id');
+            $table->integer('report_id')->nullable()->comment('汇报关系id');
+            $table->integer('level_id')->nullable()->comment('级别id');
             $table->rememberToken();
             $table->timestamps();
         });
